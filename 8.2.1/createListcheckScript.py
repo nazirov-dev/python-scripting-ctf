2cec22d4405da33f77196dee014f6ffac416ad64
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-21 20:13:31 +0500

    update

--- 8.2.1/createListcheckScript.py
+++ 8.2.1/createListcheckScript.py
@@ -12,10 +12,10 @@ while True:
     if os.path.exists(script_path):
         try:
             result = subprocess.run(["python3", script_path], capture_output=True, text=True)
-
             # Agar skript muvaffaqiyatli bajarilsa, flag faylini yaratish
-            result = result.stdout.strip().lower()
-            if result.returncode == 0 and (result == '["guts", "griffith", "casca"]' or result == "['guts', 'griffith', 'casca']"):
+            result_text = result.stdout.strip().lower()
+            print(result_text)
+            if result.returncode == 0 and (result_text == '["guts", "griffith", "casca"]' or result_text == "['guts', 'griffith', 'casca']"):
             # flag faylini yaratish
                 if not os.path.exists(flag_dir):
                     os.makedirs(flag_dir)  # Agar flag papkasi mavjud bo'lmasa, yaratish
