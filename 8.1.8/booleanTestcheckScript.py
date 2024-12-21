7b1f60dd5f4e7944888956863a5296f8e3cdfebb
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-21 18:34:06 +0500

    update

--- 8.1.8/booleanTestcheckScript.py
+++ 8.1.8/booleanTestcheckScript.py
@@ -1,10 +1,10 @@
 import subprocess
 import os
-import time
+
 # Tekshiradigan faylning yo'li
-script_path = "/home/haady/urlSlice.py"
+script_path = "/home/haady/booleanTest.py"
 flag_dir = "/home/haady/flags"
-flag_file = os.path.join(flag_dir, "urlSlice-flag.txt")
+flag_file = os.path.join(flag_dir, "booleanTest-Flag.txt")
 
 # Infinite loop to check file and write flag
 while True:
@@ -14,14 +14,14 @@ while True:
             result = subprocess.run(["python3", script_path], capture_output=True, text=True)
 
             # Agar skript muvaffaqiyatli bajarilsa, flag faylini yaratish
-            if result.returncode == 0 and result.stdout.strip() == "https://www.offensive-security.cam/offsec/game-hacking-intro/":
+            if result.returncode == 0 and result.stdout.strip() == "The sky is blue":
                 # flag faylini yaratish
                 if not os.path.exists(flag_dir):
                     os.makedirs(flag_dir)  # Agar flag papkasi mavjud bo'lmasa, yaratish
                 with open(flag_file, "w") as file:
-                    file.write("PYTHON{URLlarni_Bo'lish_va_Tartibga_Solish}\n\n")
+                    file.write("PYTHON{Bu_Raleigh_tarqalishi_tufayli_yuz_beradi}\n")
                 break
         except Exception as e:
             pass
-            
-    time.sleep(60)
+
+
