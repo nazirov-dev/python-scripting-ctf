412c29aedf5ff7f86fa1763eb8e876a498ea403a
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-21 17:46:38 +0500

    update

--- 8.1.5/urlSlice.py
+++ 8.1.5/urlSlice.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-hrefLine = '<a herf="htts://www.offensive-security.cam/offsec/game-hacking-intro/">Introduction to Game Hacking</a>'
+hrefLine = '<a herf="https://www.offensive-security.cam/offsec/game-hacking-intro/">Introduction to Game Hacking</a>'
 
-start="htts"
-end="\">"
+start=""
+end=""
 
 url=hrefLine[hrefLine.index(start):hrefLine.index(end)]
 print(url)
+
