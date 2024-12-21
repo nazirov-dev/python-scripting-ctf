128409af743abb6c139b0a8c84b893eaa57a62ca
Abdulaziz Nazirov <nazirovdev@gmail.com>
2024-12-21 16:56:41 +0500

    update

--- 8.1.2/firstScriptcheckScript.py
+++ 8.1.2/firstScriptcheckScript.py
@@ -4,7 +4,7 @@ import time
 
 # Tekshiradigan faylning yo'li
 script_path = "/home/haady/firstScript.py"
-flag_dir = "/home/haady/flag"
+flag_dir = "/home/haady/flags"
 flag_file = os.path.join(flag_dir, "firstScript-flag.txt")
 
 # Agar fayl mavjud bo'lsa, skriptni ishga tushirish
@@ -19,7 +19,7 @@ if os.path.exists(script_path):
                 if not os.path.exists(flag_dir):
                     os.makedirs(flag_dir)  # Agar flag papkasi mavjud bo'lmasa, yaratish
                 with open(flag_file, "w") as file:
-                    file.write("PYTHON{B1Rinchi_Skript_Bajarildi!}")
+                    file.write("PYTHON{B1Rinchi_Skript_Bajarildi!}\n")
                 break
         except Exception as e:
             pass
